8e0095471538c098e3f15f010b2737fddc92445e
Matheus Silveira <theunewba@gmail.com>
2025-04-01 21:44:12 -0300

    construção do card de tarefa

---
+++ src/containers/BarraLateral/index.tsx
@@ -0,0 +1,21 @@
+import FiltroCard from '../../components/FiltroCard'
+
+import * as S from './styles'
+
+const BarraLateral = () => (
+  <S.Aside>
+    <div>
+      <S.Campo type="text" placeholder="Procurar" />
+    </div>
+    <S.Filtros>
+      <FiltroCard legenda="pendentes" contador={1} />
+      <FiltroCard legenda="concluidas" contador={2} />
+      <FiltroCard legenda="urgentes" contador={3} />
+      <FiltroCard legenda="importantes" contador={4} />
+      <FiltroCard legenda="normal" contador={5} />
+      <FiltroCard legenda="todas" contador={15} ativo />
+    </S.Filtros>
+  </S.Aside>
+)
+
+export default BarraLateral
